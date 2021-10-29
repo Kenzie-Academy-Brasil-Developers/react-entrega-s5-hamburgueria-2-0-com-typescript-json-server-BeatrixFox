eba8afb8951421946abfa13972147c9bd95eab7f
BeatrixDev <beatrix.ford@hotmail.com>
2021-10-29 19:20:21 -0300

    feat: add code file register

--- src/providers/register/index.tsx
+++ src/providers/register/index.tsx
@@ -4,59 +4,49 @@ import api from "./../../services/api";
 import { History } from "history";
 import { toast } from "react-toastify";
 
-interface AuthProps {
+interface RegisterProps {
   children: ReactNode;
 }
 
 interface UserData {
   email: string;
   password: string;
+  checkPassword: string;
 }
 
 interface AuthProviderData {
   authToken: string;
-  signIn: (userData: UserData, history: History) => void;
-  Logout: (history: History) => void;
+  register: (userData: UserData, history: History) => void;
 }
 
 export const AuthContext = createContext<AuthProviderData>(
   {} as AuthProviderData
 );
 
-export const AuthProvider = ({ children }: AuthProps) => {
+export const AuthProvider = ({ children }: RegisterProps) => {
   const history = useHistory();
 
   const [authToken, setAuthToken] = useState(
     () => localStorage.getItem("token") || ""
   );
 
-  const signIn = (userData: UserData, history: History) => {
+  const register = (userData: UserData, history: History) => {
     api
-      .post("/login", userData)
+      .post("/register", userData)
       .then((response) => {
         localStorage.setItem("token", response.data.token);
 
         setAuthToken(response.data.token);
 
-        toast.success("Login Efetuado com sucesso");
+        toast.success("Registro Efetuado com sucesso");
 
         history.push("/dashboard");
       })
-      .catch((err) => toast.error("Problemas na autentificação"));
-  };
-
-  const Logout = (history: History) => {
-    localStorage.clear();
-
-    setAuthToken("");
-
-    history.push("/");
-
-    toast.info("Logout realizado");
+      .catch((err) => toast.error("Problemas de cadastro"));
   };
 
   return (
-    <AuthContext.Provider value={{ authToken, Logout, signIn }}>
+    <AuthContext.Provider value={{ authToken, register }}>
       {children}
     </AuthContext.Provider>
   );
